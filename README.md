0225b1b67eb2cbe9cb9f8390cfb29c47000eaa86
Kasper <kasper.andreasen@gmail.com>
2022-02-14 13:03:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# New_RPG_Project
+ The initial project got cluttered, trying again from scratch
---
+++ src/dk/qitsuk/Main.java
@@ -0,0 +1,8 @@
+package dk.qitsuk;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
